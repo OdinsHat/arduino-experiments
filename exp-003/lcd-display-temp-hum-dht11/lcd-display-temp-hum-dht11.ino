003c5a028d599fddb6fb2f0564fe078dbdc80a3d
Doug <doug@tintophat.com>
2018-04-13 01:32:14 +0100

    Showing temperature and humidity on a Keyes LCD display
    This experiment uses a DHT11 to read the temperature and humidity every 2 seconds and then displays thse values on a 2 line Keyes LCD display.  A keyes display has a built in LCM1602 shown in the Fritzing diagram.

---
+++ exp-003/lcd-display-temp-hum-dht11/lcd-display-temp-hum-dht11.ino
@@ -0,0 +1,64 @@
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+// Used the following DHT library: @link https://github.com/adafruit/DHT-sensor-library
+#include <DHT.h>
+#include <DHT_U.h>
+
+#include "DHT.h"
+
+#define DHTPIN 4
+#define DHTTYPE DHT11
+
+LiquidCrystal_I2C lcd(0x27,16,2);
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup()
+{
+  // Initialise the serial console
+  Serial.begin(9600);
+
+  // Initialize the lcd
+  lcd.init();
+
+  // Turn on LCD backlight.
+  lcd.backlight();
+
+  // Initialise the DHT sensor
+  dht.begin();
+}
+
+void loop()
+{
+  // Create a delay of 2 seconds to give the sensor 
+  // time to get the sensor values. 
+  delay(2000);
+  
+  float h = dht.readHumidity();
+  Serial.println(h);
+
+  float t = dht.readTemperature();
+  Serial.println(t);
+
+  float f = dht.readTemperature(true);
+  Serial.println(f);
+
+  if (isnan(h) || isnan(t) || isnan(f)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+
+  float hif = dht.computeHeatIndex(f, h);
+  float hic = dht.computeHeatIndex(t, h, false);
+
+  // Show temperature on second line
+  lcd.setCursor(0, 0);
+  lcd.print(t);
+  lcd.setCursor(6, 0);
+  lcd.print(" C");
+  
+  // Show humidity on first line
+  lcd.setCursor(0, 1);
+  lcd.print(h);
+  lcd.setCursor(6, 1);
+  lcd.print(" Humidity");
+}
